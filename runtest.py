c2588ce374f07aa819a125f8e1cb472158e0377d
Pzhe <zheair923@gmail.com>
2016-05-03 19:31:09 +0800

    Fix Timeover bug

--- runtest.py
+++ runtest.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+
 import os
 import re
 import glob
 import subprocess
+import time
 
 class Tester:
-    SERVERDIR="/home/pzhe/Desktop/planner/Planner-release-2.07-src/bin" #Server Dir
-    CLIENTDIR="/home/pzhe/Desktop/planner/Planner-release-2.07-src/bin" #Client Dir
+    SERVERDIR="" #Server Dir
+    CLIENTDIR="" #Client Dir
 
     def __init__(self, mode, stage, pronum):
         self.pronum = pronum
@@ -19,21 +21,22 @@ class Tester:
         self.dirmap = teams
 
     def runtest(self):
-        cnt = 1
-        totscore = 0
+        cnt=1
+        totscore=0
         dircreat="mkdir log_{0}_{1}/".format(self.mode, self.stage)
         os.chdir(self.SERVERDIR)
+        os.system("cp -p ../res/iclingo ../res/*.lp .")
         if not os.path.exists("log/log_{0}_{1}".format(self.mode, self.stage)):
          	os.system(dircreat)
         while cnt<=self.pronum:
-            servercmd = "./cserver -td ../tests/sim@home13.demo/stage.II \
+            servercmd = "./cserver -td ../tests/it-stage1 \
             -mode {0}  -to 5000 -test {2} -eval ../lib/libasp -log log_{0}_{1}/{2} ".format(self.mode, self.stage, cnt)
             os.chdir(self.SERVERDIR)
             if not os.path.exists("log_{0}_{1}/{2}".format(self.mode, self.stage,cnt)):
                 os.mkdir("log_{0}_{1}/{2}".format(self.mode, self.stage,cnt))
             os.system("pkill cserver")
             cserver = subprocess.Popen(servercmd,shell=True)
-
+            
             for client in self.dirmap:
                 os.chdir(self.CLIENTDIR)
                 os.system("sleep 3")
@@ -55,14 +58,14 @@ class Tester:
             	    elif line.startswith("# Score:"):
             	     tscore = line.split(':')[1].strip()
             totscore=totscore+int(tscore)
-            if cnt == self.pronum :
+            if cnt == self.pronum :  
             	print "{0} : \t  {1}".format(teamname, totscore)
             	outf = open("../{2}_{0}_{1}_res.txt".format(self.mode, self.stage,self.dirmap[client]), 'w')
             	outf.write("{0}\t:{1}\n".format(teamname, totscore))
             	outf.close()
             cnt=cnt+1
-
-
+  
+        
 
     def testall(self):
         self.runtest()
